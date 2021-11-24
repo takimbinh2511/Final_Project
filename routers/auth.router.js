ebcf7bc8eb0e40cb69c5f52e033c5af159e55efd
binh18520510 <18520510@gm.uit.edu.vn>
2021-11-24 22:32:49 +0700

    update 5

--- routers/auth.router.js
+++ routers/auth.router.js
@@ -1,5 +1,6 @@
 // const { Router } = require("express");
 const { signIn } = require("../controllers/auth.controller");
+const { authenticate, authorize } = require("../middlewares/verifytoken");
 
 // const authRouter = Router();
 // //http://localhost:3000/api/v1/auth/sign-in
@@ -9,7 +10,11 @@ const { signIn } = require("../controllers/auth.controller");
 //     authRouter,
 // };
 
+
 module.exports = function (app) {
-    app.post("/api/v1/auth/sign-in", signIn);
+    app.post("/api/v1/auth/sign-in",signIn);
+    app.get("/api/v1/auth/", authenticate,(req, res)=>{
+        return res.send(req.user);
+    });
 };
   
